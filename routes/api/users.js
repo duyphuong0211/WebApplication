eed679e32605f6a86b295cd98eacb8456cdc79f3
duyphuong0211 <18521276@gm.uit.edu.vn>
2021-09-07 22:21:29 +0700

    User Authentication/ Login Route

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
-const {check, validationResult} = require("express-validator");
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const {check, validationResult} = require("express-validator");
+
 const User = require('../../models/User');
 
 // @route  POST api/users
