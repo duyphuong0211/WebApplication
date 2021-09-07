19db652f98388add925903afbf5a9667c8823d05
duyphuong0211 <18521276@gm.uit.edu.vn>
2021-09-07 16:51:50 +0700

    Request & Body validation with register user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
-// @route  GET api/users
-// @desc   test route
+// @route  POST api/users
+// @desc   Register user
 // @access Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // if some inf above dont map, it will return here(400 errors)
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    res.send("User route");
+  }
+);
 
 module.exports = router;
