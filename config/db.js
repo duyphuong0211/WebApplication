c90c0f1226c0fc558bb08cdfb72d8c57bc0ba166
duyphuong0211 <18521276@gm.uit.edu.vn>
2021-09-09 09:47:30 +0700

    some revamp + get all profiles and by user ID

--- config/db.js
+++ config/db.js
@@ -6,10 +6,12 @@ const connectDB = async () => {
     try {
         await mongoose.connect(db, {
             useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false
         });
 
-        console.log('mongoDB Connected...');
-    } catch(err) {
+        console.log('MongoDB Connected...');
+    } catch (err) {
         console.error(err.message);
         // Exit process with failure
         process.exit(1);
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
-const {check, validationResult} = require("express-validator");
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const config = require("config");
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
 
-// @route  GET api/auth
-// @desc   Test route
-// @access Public
+// @route    GET api/auth
+// @desc     Test route
+// @access   Public
 router.get('/', auth, async (req, res) => {
     try {
-        const user = await User.findById(req.user.id).select(('-password'));
+        const user = await User.findById(req.user.id).select('-password');
         res.json(user);
     } catch (err) {
         console.error(err.message);
@@ -22,63 +21,59 @@ router.get('/', auth, async (req, res) => {
     }
 });
 
-// @route  POST api/auth
-// @desc   Authenticate user & get token
-// @access Public
+// @route    POST api/auth
+// @desc     Authenticate user & get token
+// @access   Public
 router.post(
-    "/",
+    '/',
     [
-        check("email", "Please include a valid email").isEmail(),
-        check(
-            "password",
-            "Password is requried"
-        ).exists()
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Password is required').exists()
     ],
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            // if some inf above dont map, it will return here(400 errors)
-            return res.status(400).json({errors: errors.array()});
+            return res.status(400).json({ errors: errors.array() });
         }
 
-        const { email, password} = req.body;
+        const { email, password } = req.body;
 
         try {
             let user = await User.findOne({ email });
 
-            if(!user) {
-                res.status(400)
-                    .json({ errors: [{ msg: 'Invalid Credentials'}]});
+            if (!user) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'Invalid Credentials' }] });
             }
 
             const isMatch = await bcrypt.compare(password, user.password);
 
-            if(!isMatch) {
-                res.status(400)
-                    .json({ errors: [{ msg: 'Invalid Credentials'}]});
+            if (!isMatch) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'Invalid Credentials' }] });
             }
 
-            // Return jsonwebtoken
             const payload = {
                 user: {
-                    id: user._id
+                    id: user.id
                 }
-            }
+            };
 
             jwt.sign(
                 payload,
                 config.get('jwtSecret'),
-                { expiresIn: 36000},
+                { expiresIn: 360000000000000000000 },
                 (err, token) => {
-                    if(err) throw err;
+                    if (err) throw err;
                     res.json({ token });
                 }
             );
         } catch (err) {
             console.error(err.message);
-            res.status(500).send('Server error')
+            res.status(500).send('Server error');
         }
-        console.log(req.body);
     }
 );
 
