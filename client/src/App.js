01da3b984f47dcb794c9eeabefad9f9580454c1b
duyphuong0211 <18521276@gm.uit.edu.vn>
2021-10-16 22:11:46 +0700

    Alert system complete

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,11 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
-
 import './App.css';
-
 const App = () => (
     <Provider store={store}>
         <Router>
@@ -17,6 +16,7 @@ const App = () => (
                 <Navbar />
                 <Route exact path='/' component={Landing} />
                 <section className='container'>
+                    <Alert />
                     <Switch>
                         <Route exact path='/register' component={Register} />
                         <Route exact path='/login' component={Login} />
@@ -26,5 +26,4 @@ const App = () => (
         </Router>
     </Provider>
 );
-
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -11,16 +14,14 @@ const Register = () => {
     const { name, email, password, password2 } = formData;
     const onChange = e =>
         setFormData({ ...formData, [e.target.name]: e.target.value });
-
     const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
-            console.log('Passwords do not match');
+            setAlert('Passwords do not match', 'danger');
         } else {
             console.log('SUCCESS');
         }
     };
-
     return (
         <Fragment>
             <h1 className='large text-primary'>Sign Up</h1>
@@ -80,4 +81,12 @@ const Register = () => {
         </Fragment>
     );
 };
-export default Register;
+
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
+};
+
+export default connect(
+    null,
+    { setAlert }
+)(Register);
