21326e5d0ec297e14293b64b607704bef8c2618d
duyphuong0211 <18521276@gm.uit.edu.vn>
2021-10-21 21:35:05 +0700

    Protected route, profile reducer and getCurrentProfile action

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,21 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
-
 import './App.css';
-
 if (localStorage.token) {
     setAuthToken(localStorage.token);
 }
-
 const App = () => {
     useEffect(() => {
         store.dispatch(loadUser());
     }, []);
-
     return (
         <Provider store={store}>
             <Router>
@@ -33,6 +31,7 @@ const App = () => {
                         <Switch>
                             <Route exact path='/register' component={Register} />
                             <Route exact path='/login' component={Login} />
+                            <PrivateRoute exact path='/dashboard' component={Dashboard} />
                         </Switch>
                     </section>
                 </Fragment>
@@ -40,5 +39,4 @@ const App = () => {
         </Provider>
     );
 };
-
 export default App;
