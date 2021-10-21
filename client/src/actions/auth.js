43cb4fabecf49b1c5545f80616fee4ac181c9f87
duyphuong0211 <18521276@gm.uit.edu.vn>
2021-10-21 21:02:12 +0700

    Logout & navbar links

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_SUCCESS,
-    LOGIN_FAIL
+    LOGIN_FAIL,
+    LOGOUT
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -41,11 +42,9 @@ export const register = ({ name, email, password }) => async dispatch => {
             type: REGISTER_SUCCESS,
             payload: res.data
         });
-
         dispatch(loadUser());
     } catch (err) {
         const errors = err.response.data.errors;
-
         if (errors) {
             errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
         }
@@ -54,7 +53,6 @@ export const register = ({ name, email, password }) => async dispatch => {
         });
     }
 };
-
 // Login User
 export const login = (email, password) => async dispatch => {
     const config = {
@@ -62,27 +60,26 @@ export const login = (email, password) => async dispatch => {
             'Content-Type': 'application/json'
         }
     };
-
     const body = JSON.stringify({ email, password });
-
     try {
         const res = await axios.post('/api/auth', body, config);
-
         dispatch({
             type: LOGIN_SUCCESS,
             payload: res.data
         });
-
         dispatch(loadUser());
     } catch (err) {
         const errors = err.response.data.errors;
-
         if (errors) {
             errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
         }
-
         dispatch({
             type: LOGIN_FAIL
         });
     }
+};
+
+// Logout / Clear Profile
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT });
 };
