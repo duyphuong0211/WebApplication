26fcd8e0117d64fff219c92b0624f219ce8256ff
duyphuong0211 <18521276@gm.uit.edu.vn>
2021-10-27 16:07:32 +0700

    CreateProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -17,8 +17,10 @@ export const loadUser = () => async dispatch => {
     if (localStorage.token) {
         setAuthToken(localStorage.token);
     }
+
     try {
         const res = await axios.get('/api/auth');
+
         dispatch({
             type: USER_LOADED,
             payload: res.data
@@ -29,6 +31,7 @@ export const loadUser = () => async dispatch => {
         });
     }
 };
+
 // Register User
 export const register = ({ name, email, password }) => async dispatch => {
     const config = {
@@ -36,24 +39,31 @@ export const register = ({ name, email, password }) => async dispatch => {
             'Content-Type': 'application/json'
         }
     };
+
     const body = JSON.stringify({ name, email, password });
+
     try {
         const res = await axios.post('/api/users', body, config);
+
         dispatch({
             type: REGISTER_SUCCESS,
             payload: res.data
         });
+
         dispatch(loadUser());
     } catch (err) {
         const errors = err.response.data.errors;
+
         if (errors) {
             errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
         }
+
         dispatch({
             type: REGISTER_FAIL
         });
     }
 };
+
 // Login User
 export const login = (email, password) => async dispatch => {
     const config = {
@@ -61,19 +71,25 @@ export const login = (email, password) => async dispatch => {
             'Content-Type': 'application/json'
         }
     };
+
     const body = JSON.stringify({ email, password });
+
     try {
         const res = await axios.post('/api/auth', body, config);
+
         dispatch({
             type: LOGIN_SUCCESS,
             payload: res.data
         });
+
         dispatch(loadUser());
     } catch (err) {
         const errors = err.response.data.errors;
+
         if (errors) {
             errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
         }
+
         dispatch({
             type: LOGIN_FAIL
         });
