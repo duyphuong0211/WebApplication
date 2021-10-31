83b7a03b664b7f53ef238a5b1ed86b18e7139527
duyphuong0211 <18521276@gm.uit.edu.vn>
2021-10-31 21:30:12 +0700

    Delete experiences, education and account

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import Experience from './Experience';
 import Education from './Education';
-import { getCurrentProfile } from '../../actions/profile';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 
 
 const Dashboard = ({
@@ -30,6 +30,12 @@ const Dashboard = ({
                     <DashboardActions />
                     <Experience experience={profile.experience} />
                     <Education education={profile.education} />
+
+                    <div className='my-2'>
+                        <button className='btn btn-danger' onClick={() => deleteAccount()}>
+                            <i className='fas fa-user-minus' /> Delete My Account
+                        </button>
+                    </div>
                 </Fragment>
             ) : (
                 <Fragment>
@@ -45,6 +51,7 @@ const Dashboard = ({
 
 Dashboard.propTypes = {
     getCurrentProfile: PropTypes.func.isRequired,
+    deleteAccount: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     profile: PropTypes.object.isRequired
 };
@@ -54,5 +61,5 @@ const mapStateToProps = state => ({
 });
 export default connect(
     mapStateToProps,
-    { getCurrentProfile }
+    { getCurrentProfile, deleteAccount }
 )(Dashboard);
