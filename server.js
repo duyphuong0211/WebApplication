973c33be9b13528283c2aaf92b856f28956b4c34
Duy Phương Nguyên <79742795+duyphuong0211@users.noreply.github.com>
2021-11-28 22:24:04 +0700

    Update server.js
    update

--- server.js
+++ server.js
@@ -3,12 +3,9 @@ const connectDB = require('./config/db');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const path = require('path');
-
 const app = express();
-
 // Connect Database
 connectDB();
-
 // Init Middleware
 app.use(express.json({ extended: false }));
 
@@ -21,17 +18,30 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
-
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
-
 const PORT = process.env.PORT || 5000;
+const swaggerOptions = {
+        swaggerDefinition: {
+            info: {
+                title: "API",
+                description: "API Information",
+                contact: {
+                    name: "Duy Phuong"
+                },
+                server: ["http://localhost:5000"]
+            }
+        },
+    apis: ["app.js"]
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+console.log(swaggerDocs);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
